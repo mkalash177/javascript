88029e6559ebce7fd80643b2e7ff6e6da1e94983
Mikhail <mkalash177@gmail.com>
2019-11-15 02:20:03 +0200

    qwerty

--- HM/02.js
+++ HM/02.js
@@ -12,3 +12,29 @@ let str = "abcde"
 alert(str[0])
 alert(str[1])
 alert(str[str.length-1])
+for(x of str){
+    if(x === 'a'){
+        console.log("я знаю эту букву")
+    }else if(x === 'b' ){
+        console.log("я знаю эту букву")
+    }else if(x === 'e'){
+        console.log('я знаю эту букву')
+    }else{
+        console.log(x)
+    }
+}
+//03
+let hour = prompt("Который час")
+let hoursec = 60 * 60 * Number(hour)
+alert('В ' + hour + " часах " + hoursec + " секунд")
+//04
+var hour = Number(prompt("Который час"))
+while(true){
+    if(hour !== Number(hour)){
+        var hour = Number(prompt("не верный формат, пробуйте еще"))
+    }else if(hour === Number(hour)){
+        break
+    }
+}
+let hoursec = 60 * 60 * Number(hour)
+alert('В ' + hour + " часах " + hoursec + " секунд")
